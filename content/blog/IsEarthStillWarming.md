caee100cc5103f6bc28211325436c7d96c45b6aa
Ross Hill <ross@rosshill.ca>
2019-02-05 19:17:31 -0500

    get blog posts generating and start building landing page

---
+++ content/blog/IsEarthStillWarming.md
@@ -0,0 +1,14 @@
+---
+title: IsEarthStillWarming.com
+date: 2018-01-01
+website: https://isearthstillwarming.com
+repository: https://github.com/rosslh/isearthstillwarming.com
+---
+
+## Header
+
+This year, I'm on the QHack's technology team, building internal tools and improving our online presence. QHacks is a hackathon hosted at Queen’s University where software developers come together to collaborate on projects and win prizes. I am helping to create a landing page for the event and the “QHacks Dashboard”, an online platform for QHacks participants. Through building these and other tools, I aim help the QHacks team run a smooth and successful event!
+
+More text and stuff goes here.
+
+Blah Blah Blah
--- src/routes/blog/[slug].json.js
+++ src/routes/blog/[slug].json.js
@@ -1,28 +1,44 @@
-import posts from './_posts.js';
+import slugify from "slugify";
+
+const blog = require("../../../content/blog.json");
+
+const posts = blog.map(post => {
+  return {
+    title: post.attributes.title,
+    slug: slugify(post.attributes.title),
+    date: post.attributes.date,
+    website: post.attributes.website,
+    website: post.attributes.repository,
+    content: post.body,
+    image: post.attributes.image
+  };
+});
 
 const lookup = new Map();
 posts.forEach(post => {
-	lookup.set(post.slug, JSON.stringify(post));
+  lookup.set(post.slug.toLowerCase(), JSON.stringify(post));
 });
 
 export function get(req, res, next) {
-	// the `slug` parameter is available because
-	// this file is called [slug].json.js
-	const { slug } = req.params;
+  // the `slug` parameter is available because
+  // this file is called [slug].json.js
+  const slug = req.params.slug.toLowerCase();
 
-	if (lookup.has(slug)) {
-		res.writeHead(200, {
-			'Content-Type': 'application/json'
-		});
+  if (lookup.has(slug)) {
+    res.writeHead(200, {
+      "Content-Type": "application/json"
+    });
 
-		res.end(lookup.get(slug));
-	} else {
-		res.writeHead(404, {
-			'Content-Type': 'application/json'
-		});
+    res.end(lookup.get(slug));
+  } else {
+    res.writeHead(404, {
+      "Content-Type": "application/json"
+    });
 
-		res.end(JSON.stringify({
-			message: `Not found`
-		}));
-	}
+    res.end(
+      JSON.stringify({
+        message: `Not found`
+      })
+    );
+  }
 }
