13834e9ee5f6a09dd38c879503f802d376c7d0ff
Ross Hill <ross@rosshill.ca>
2019-02-05 19:17:31 -0500

    get blog posts generating and start building landing page

---
+++ src/routes/index.json.js
@@ -0,0 +1,41 @@
+import slugify from "slugify";
+
+const blog = require("../../../content/blog.json"); // TODO: Why is this not ../../
+
+const extractFirstPara = content => {
+  const xmldom = require("xmldom");
+  const { DOMParser, XMLSerializer } = xmldom;
+  const parser = new DOMParser();
+  const serializer = new XMLSerializer();
+  const doc = parser.parseFromString(content, "text/html");
+  return serializer.serializeToString(doc.getElementsByTagName("p")[0]);
+};
+
+const contents = blog.map(post => {
+  return {
+    title: post.attributes.title,
+    slug: slugify(post.attributes.title),
+    date: post.attributes.date,
+    website: post.attributes.website,
+    website: post.attributes.repository,
+    content: extractFirstPara(post.body),
+    image: post.attributes.image ? `blog/${post.attributes.image}` : null
+  };
+});
+
+const filterPosts = (posts, searchString) =>
+  posts.filter(
+    post =>
+      post.title.toLowerCase().includes(searchString) ||
+      post.content.toLowerCase().includes(searchString)
+  );
+
+export function get(req, res) {
+  res.writeHead(200, {
+    "Content-Type": "application/json"
+  });
+  const output = JSON.stringify(
+    filterPosts(contents, req.query.searchString.toLowerCase())
+  );
+  res.end(output);
+}
