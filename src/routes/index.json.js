bbc713b84abd051ae81f1817deb103de19996066
Ross Hill <ross@rosshill.ca>
2019-02-07 00:53:09 -0500

    Clean up

--- src/routes/index.json.js
+++ src/routes/index.json.js
@@ -25,19 +25,59 @@ const contents = blog.map(post => {
   };
 });
 
-const filterPosts = (posts, searchString) =>
-  posts.filter(
-    post =>
-      post.title.toLowerCase().includes(searchString) ||
-      post.content.toLowerCase().includes(searchString)
-  );
+const filterPosts = (posts, searchString) => {
+  if (searchString) {
+    posts = posts.filter(
+      post =>
+        post.title.toLowerCase().includes(searchString) ||
+        post.content.toLowerCase().includes(searchString)
+    );
+  }
+  return posts;
+};
+
+const shuffle = arr => {
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+};
+
+const compare = (a, b, sort) => {
+  switch (sort) {
+    case "alpha": {
+      if (a.title < b.title) return -1;
+      if (a.title > b.title) return 1;
+      else return 0;
+    }
+    case "revalpha": {
+      if (a.title > b.title) return -1;
+      if (a.title < b.title) return 1;
+      else return 0;
+    }
+    case "old": {
+      return new Date(a.date) - new Date(b.date);
+    }
+    default: {
+      // new
+      return new Date(b.date) - new Date(a.date);
+    }
+  }
+};
 
 export function get(req, res) {
   res.writeHead(200, {
     "Content-Type": "application/json"
   });
-  const output = JSON.stringify(
-    filterPosts(contents, req.query.searchString.toLowerCase())
-  );
-  res.end(output);
+  const searchString = req.query.searchString.toLowerCase() || "";
+  const sort = req.query.sort.toLowerCase() || "";
+
+  const filtered = filterPosts(contents, searchString);
+
+  let output;
+
+  if (sort.toLowerCase() === "shuffle") output = shuffle(filtered);
+  else output = filtered.sort((a, b) => compare(a, b, sort));
+
+  res.end(JSON.stringify(output));
 }
