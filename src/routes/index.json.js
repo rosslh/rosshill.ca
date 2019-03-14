ed31ec34ad4ae95ca6d1d6d4fd8ac5de67add74b
Ross Hill <rosshill@protonmail.com>
2019-03-14 18:50:11 -0400

    Update projects Simplify homepage

--- src/routes/index.json.js
+++ src/routes/index.json.js
@@ -1,30 +1,40 @@
 import slugify from "slugify";
 import dateformat from "dateformat";
+import generate from "string-to-color";
+import { setLightness, setSaturation } from "unitransform";
 
 const projects = require("../../../content/projects.json"); // TODO: Why is this not ../../
 
-const extractFirstPara = content => {
-  const xmldom = require("xmldom");
-  const { DOMParser, XMLSerializer } = xmldom;
-  const parser = new DOMParser();
-  const serializer = new XMLSerializer();
-  const doc = parser.parseFromString(content, "text/html");
-  return serializer.serializeToString(doc.getElementsByTagName("p")[0]);
-};
+// const extractFirstPara = content => {
+//   const xmldom = require("xmldom");
+//   const { DOMParser, XMLSerializer } = xmldom;
+//   const parser = new DOMParser();
+//   const serializer = new XMLSerializer();
+//   const doc = parser.parseFromString(content, "text/html");
+//   return serializer.serializeToString(doc.getElementsByTagName("p")[0]);
+// };
 
 const contents = projects
   .filter(x => !x.attributes.WIP)
   .map(post => {
     return {
       title: post.attributes.title,
-      tags: post.attributes.tags.map(x => x.toLowerCase()).sort(),
+      tags: post.attributes.tags
+        .map(x => ({
+          text: x.toLowerCase(),
+          color: setSaturation(setLightness(generate(x.toLowerCase()), 65), 80)
+        }))
+        .sort(),
       slug: slugify(post.attributes.title).toLowerCase(),
       date: post.attributes.date,
       prettyDate: dateformat(post.attributes.date, "mmmm yyyy"),
       website: post.attributes.website,
       repository: post.attributes.repository,
-      content: extractFirstPara(post.body),
-      image: post.attributes.image ? `projects/${post.attributes.image}` : null
+      blurb: post.attributes.blurb,
+      image: post.attributes.image ? `projects/${post.attributes.image}` : null,
+      thumbnail: post.attributes.thumbnail
+        ? `projects/${post.attributes.thumbnail}`
+        : null
     };
   })
   .sort((a, b) => new Date(b.date) - new Date(a.date));
