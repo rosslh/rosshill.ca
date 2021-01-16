c0ab335366333ccf302aa2a01323175cbe0764c9
Ross Hill <ross@rosshill.ca>
2021-01-16 18:14:39 -0500

    Styling changes Remove image alpha channel

--- src/components/PostArrow.svelte
+++ src/components/PostArrow.svelte
@@ -2,6 +2,14 @@
   export let left;
 </script>
 
+{#if left}
+  <div aria-hidden="true" class="arrowLeft" />
+  <div aria-hidden="true" class="arrowLeftBorder" />
+{:else}
+  <div aria-hidden="true" class="arrowRight" />
+  <div aria-hidden="true" class="arrowRightBorder" />
+{/if}
+
 <style>
   .arrowRight {
     z-index: 6;
@@ -20,13 +28,13 @@
     z-index: 2;
     width: 0;
     height: 0;
-    border-top: 18px solid transparent;
-    border-bottom: 18px solid transparent;
+    border-top: 18.5px solid transparent;
+    border-bottom: 18.5px solid transparent;
     position: absolute;
     top: 50%;
     transform: translate(50%, -50%);
     right: 1.7rem;
-    border-left: 18px solid var(--postBorder);
+    border-left: 18.5px solid var(--postBorder);
   }
 
   .arrowLeft {
@@ -46,20 +54,12 @@
     z-index: 2;
     width: 0;
     height: 0;
-    border-top: 18px solid transparent;
-    border-bottom: 18px solid transparent;
+    border-top: 18.5px solid transparent;
+    border-bottom: 18.5px solid transparent;
     position: absolute;
     top: 50%;
     transform: translate(-50%, -50%);
     left: 1.7rem;
-    border-right: 18px solid var(--postBorder);
+    border-right: 18.5px solid var(--postBorder);
   }
 </style>
-
-{#if left}
-  <div aria-hidden="true" class="arrowLeft" />
-  <div aria-hidden="true" class="arrowLeftBorder" />
-{:else}
-  <div aria-hidden="true" class="arrowRight" />
-  <div aria-hidden="true" class="arrowRightBorder" />
-{/if}
