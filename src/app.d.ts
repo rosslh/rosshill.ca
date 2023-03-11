6bee59d62c8d905f29127c373877c44d913b8661
Antoine Greuzard <antoine@antoinegreuzard.fr>
2023-03-11 17:41:30 +0100

    ADD node server

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,11 +2,8 @@ import type { SiteTheme } from "$lib/types";
 
 declare global {
   namespace App {
-    // interface Error {}
     interface Locals {
       theme: SiteTheme
     }
-    // interface PageData {}
-    // interface Platform {}
   }
 }
--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,16 @@ import { sveltekit } from "@sveltejs/kit/vite";
 import Icons from "unplugin-icons/vite";
 import { resolve } from "path";
 import type { UserConfig } from "vite";
+import netlifyPlugin from "@sveltejs/adapter-netlify";
 
 const config: UserConfig = {
   plugins: [
     sveltekit(),
     Icons({
       compiler: "svelte",
+    }),
+    netlifyPlugin({
+      publish: "build",
     }),
   ],
   resolve: {
