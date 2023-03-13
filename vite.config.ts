09761c0c492f29cbfdbaf060657e2e5a93146325
Antoine Greuzard <antoine@antoinegreuzard.fr>
2023-03-13 09:02:09 +0100

    FIX pwa "purpose": "maskable"

--- vite.config.ts
+++ vite.config.ts
@@ -32,15 +32,11 @@ const config: UserConfig = {
             "src": "/android-chrome-192x192.png",
             "sizes": "192x192",
             "type": "image/png",
+            "purpose": "maskable",
           },
           {
             "src": "/android-chrome-512x512.png",
             "sizes": "512x512",
-            "type": "image/png",
-          },
-          {
-            "src": "/maskable-icon.png",
-            "sizes": "196x196",
             "type": "image/png",
             "purpose": "maskable",
           },
