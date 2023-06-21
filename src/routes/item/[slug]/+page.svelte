c9c5c81d0b4e31e6a3e5337b01fea074f64b467a
Antoine Greuzard <antoine@antoinegreuzard.fr>
2023-06-21 10:07:36 +0200

    UPDATE traduis

--- src/routes/item/[slug]/+page.svelte
+++ src/routes/item/[slug]/+page.svelte
@@ -81,7 +81,7 @@
         {#if post.website}
           <InlineSeparator />
           <a target="_blank" rel="noopener noreferrer" href={post.website}>
-            Website
+            Site web
           </a>
         {/if}
       </div>
