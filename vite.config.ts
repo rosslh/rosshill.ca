1e5d0f8f68a32ddec82e6ef72abf5527c2bfaef9
antoinegreuzard <antoine@antoinegreuzard.fr>
2023-06-24 16:50:00 +0200

    FIX vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { sveltekit } from "@sveltejs/kit/vite";
 import Icons from "unplugin-icons/vite";
 import { resolve } from "path";
 import type { UserConfig } from "vite";
-import netlifyPlugin from "@sveltejs/adapter-netlify";
 
 const config: UserConfig = {
   plugins: [
@@ -10,26 +9,13 @@ const config: UserConfig = {
     Icons({
       compiler: "svelte",
     }),
-    netlifyPlugin({
-      publish: "build",
-    }),
   ],
   resolve: {
     alias: {
       $data: resolve("./src/data"),
     },
   },
-  build: {
-    optimizeDeps: {
-      include: [
-        "svelte",
-        "svelte/transition",
-        "svelte/internal",
-        "svelte/store", // Add this for faster rebuilds
-      ],
-    },
-    brotliSize: true,
-  },
 };
+
 
 export default config;
