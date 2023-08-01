142bc226ff1098e28eea6778274a839954749fd3
antoinegreuzard <antoine@antoinegreuzard.fr>
2023-08-01 09:25:42 +0200

    Correct regex for escaping dollar signs in getIcons.ts
    Changed the regex in the getIcons function to correctly escape all dollar signs in the propertyName string. Previously, it was only escaping the first occurrence. This is needed to correctly match properties that may contain more than one dollar sign.

--- scripts/getIcons.ts
+++ scripts/getIcons.ts
@@ -55,7 +55,7 @@ const getNumbersFromStylesheetProperty = (
   fileLines: string[],
   propertyName: string,
 ): number[] => {
-  const escapedPropertyName = propertyName.replace("$", "\\$");
+  const escapedPropertyName = propertyName.replace(/\$/g, "\\$");
   const pattern = new RegExp(`^\\s*${escapedPropertyName}:.*;$`);
   const matches = fileLines.filter((line) => pattern.test(line));
 
